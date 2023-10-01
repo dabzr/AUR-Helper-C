8560ee01f745ee5f61aef7171286bec1f2ec557d
dylanabzr <dylanab@pm.me>
2023-10-01 15:21:00 -0300

    Improves to main function

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include "../include/aurhelper.h"
 
 int main (int argc, char* argv[]){
-  if (!strcmp(argv[1], "-S"))
-    aur_help(argv[2]);
-  else 
-    printf("Not valid parameters, try ./yay-clone -S yourapp or ./bin/yay-clone -S yourapp\n");
-  return 0;
+
+  if (strcmp(argv[1], "-S")){
+    fprintf(stderr, "Not valid parameters, try ./yay-clone -S yourapp or ./bin/yay-clone -S yourapp\n");
+    return 1;
+  }
+
+  aur_help(argv[2]);    
 }
